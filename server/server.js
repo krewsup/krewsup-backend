cc80ede1ad1d20f9030f3126c9bb70b9391c54fd
Fida Hussain <honghog120@gmail.com>
2025-11-22 00:38:26 +0530

    Change server to app for listening on PORT

--- server/server.js
+++ server/server.js
@@ -9130,4 +9130,4 @@ app.post('/api/remove-application', async (req, res) => {
 
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
